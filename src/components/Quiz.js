ec00c5c2d19dc66a50e68ba395f06b946ae8480b
Jinyeong Jung <jinyeong32@gmail.com>
2022-07-06 14:02:37 +0900

    fetch API

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,17 +1,33 @@
+import PropTypes from 'prop-types';
 import './Quiz.css';
 
-function Quiz() {
+function Quiz({ question, correctAnswer, incorrectAnswers }) {
+  let unshuffledAnswers = [correctAnswer, ...incorrectAnswers];
+  let shuffledAnswers = unshuffledAnswers
+    .map((value) => ({
+      value,
+      sort: Math.random(),
+    }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value)
+    .map((answer, i) => (
+      <li key={i} className='answer'>
+        {answer}
+      </li>
+    ));
+
   return (
     <div className='quiz'>
-      <h4 className='question'>How would one say goodbye in Spanish?</h4>
-      <ul className='answers'>
-        <li className='answer'>Adios</li>
-        <li className='answer'>Hola</li>
-        <li className='answer'>Au Revoir</li>
-        <li className='answer'>Salir</li>
-      </ul>
+      <h4 className='question'>{question}</h4>
+      <ul className='answers'>{shuffledAnswers}</ul>
     </div>
   );
 }
 
 export default Quiz;
+
+Quiz.prototype = {
+  question: PropTypes.string.isRequired,
+  correctAnswer: PropTypes.string.isRequired,
+  incorrectAnswers: PropTypes.array.isRequired,
+};
--- src/routes/QuizPage.js
+++ src/routes/QuizPage.js
@@ -3,24 +3,51 @@ import Quiz from '../components/Quiz';
 import blobYellowSrc from '../images/blob-yellow.png';
 import blobBlueSrc from '../images/blob-blue.png';
 import Button from '../components/Button';
+import { useEffect, useState } from 'react';
+import { nanoid } from 'nanoid';
 
 function QuizPage() {
+  const API_URL = 'https://opentdb.com/api.php?amount=10';
+
+  const [quizzes, setQuizzes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getQuizzes() {
+      const res = await fetch(API_URL);
+      const json = await res.json();
+      setQuizzes(json.results);
+    }
+
+    getQuizzes();
+    setLoading(false);
+  }, []);
+
+  const quizzesElements = quizzes.map((quiz) => (
+    <Quiz
+      key={nanoid()}
+      question={quiz.question}
+      correctAnswer={quiz.correct_answer}
+      incorrectAnswers={quiz.incorrect_answers}
+    />
+  ));
+
   return (
     <div className='quiz-page'>
       <img className='blobYellow' src={blobYellowSrc} alt='blob-yellow' />
       <img className='blobBlue' src={blobBlueSrc} alt='blob-blue' />
-
-      <div className='quizzes'>
-        <Quiz />
-        <Quiz />
-        <Quiz />
-        <Quiz />
-        <div className='button-container'>
-          {/* <h4>You scored 3/5 correct answers</h4> */}
-          <Button text='Check answers' />
-          {/* <Button text='Play again' /> */}
+      {loading ? (
+        <div className='loading'>Loading...</div>
+      ) : (
+        <div className='quizzes'>
+          {quizzesElements}
+          <div className='button-container'>
+            {/* <h4>You scored 3/5 correct answers</h4> */}
+            <Button text='Check answers' />
+            {/* <Button text='Play again' /> */}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
