81230155f33e374670c98d2cec99e00a50082387
Jinyeong Jung <jinyeong32@gmail.com>
2022-07-06 10:52:35 +0900

    home page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,21 @@
 import './Home.css';
 import blobYellowSrc from '../images/blob-yellow.png';
 import blobBlueSrc from '../images/blob-blue.png';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <div>
+    <div className='home'>
       <img className='blobYellow' src={blobYellowSrc} alt='blob-yellow' />
       <img className='blobBlue' src={blobBlueSrc} alt='blob-blue' />
+
+      <main>
+        <h1 className='title'>Quizzical</h1>
+        <p className='text'>Can you answer these 15 quizzical questions?</p>
+        <Link to='/quiz'>
+          <button className='quiz-btn'>Start quiz</button>
+        </Link>
+      </main>
     </div>
   );
 }
